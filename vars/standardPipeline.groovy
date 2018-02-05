f050a5b6b28b513a179f1049d40847513722619b
Dinesh <dinesh.sc@kloud9.nyc>
2018-02-05 21:40:28 +0530

    just modified this groovy 100

--- vars/standardPipeline.groovy
+++ vars/standardPipeline.groovy
@@ -22,6 +22,8 @@ pipeline {
 	   steps {
 		sh "mkdir -p output"
 		sh "cp -vaf target/*.jar output/"
+		def artifacts = archiveArtifacts artifacts: 'output/*.jar'
+		sh "echo $artifacts"
 	}
       }
     }
