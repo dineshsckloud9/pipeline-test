49c0711a6b52f88576618c555592c7798be85955
dineshsckloud9 <34510224+dineshsckloud9@users.noreply.github.com>
2018-02-09 10:31:24 +0530

    Update Maven.groovy

--- src/org/k9/build/Maven.groovy
+++ src/org/k9/build/Maven.groovy
@@ -13,55 +13,7 @@ class Maven {
   }
 
   void build() {
-	  println "Inside Maven"
-	    println(this.config.testcase)
-    if (this.config.testcase == true) {
-        if (this.config.pompath.length() > 0 && this.config.pompath != false) {
-
-            String fileName = this.config.pompath.substring(this.config.pompath.lastIndexOf("/"))
-            if(fileName.lastIndexOf(".") != -1 && fileName.lastIndexOf(".") != 0) {
-                 this.tmp = fileName.substring(fileName.lastIndexOf(".")+1);
-            } else {
-                 this.tmp = "Something else"
-            }
-	    System.out.println(this.config.pompath.length())
-            if (this.tmp != 'xml' && this.config.pompath.charAt(this.config.pompath.length()-1) != File.separatorChar) {
-                this.config.pompath += File.separator;
-            }
-
-            if (this.tmp == 'xml') {
-                return "mvn clean install -DskipTests=true -f " + this.config.pompath
-            } else {
-                return "mvn clean install -DskipTests=true -f " + this.config.pompath + "pom.xml"
-            }
-        } else {
-            return "mvn clean install -DskipTests=true"
-        }
-    } else if (this.config.testcase == false) {
-        if (this.config.pompath.length() > 0 && this.config.pompath != false) {
-
-            String fileName = this.config.pompath.substring(this.config.pompath.lastIndexOf("/"))
-            if(fileName.lastIndexOf(".") != -1 && fileName.lastIndexOf(".") != 0) {
-                 String extension = fileName.substring(fileName.lastIndexOf(".")+1);
-            } else {
-                 String extension = "Something else"
-            }
-
-            if (extension != 'xml' && this.config.pompath.charAt(this.config.pompath.length()-1) != File.separatorChar) {
-                this.config.pompath += File.separator;
-            }
-
-            if (extension == 'xml') {
-                return "mvn clean install -DskipTests=false -f " + this.config.pompath
-            } else {
-                return "mvn clean install -DskipTests=false -f " + this.config.pompath + "pom.xml"
-            }
-        } else {
-            return "mvn clean install -DskipTests=false"
-        }
-    } else {
-        this.script.error "testcase parameter has invalid value!"
-	    println(this.config.testcase)
-    }
+	  this.script.error "hhhhhhhhhhhhhhhhhhhhhhhh"
+	  println "Inside Maven"	    
   }
 }
