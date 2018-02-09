c98ad3e230f29f9122187766a5aad0ddb90eedc4
dineshsckloud9 <34510224+dineshsckloud9@users.noreply.github.com>
2018-02-09 11:01:14 +0530

    Update Maven.groovy

--- src/org/k9/build/Maven.groovy
+++ src/org/k9/build/Maven.groovy
@@ -14,35 +14,27 @@ class Maven {
 
   void build() {
    this.script.stage('Maven build') {
-	  	println(this.config.testcase)
     if (this.config.testcase == true) {
-	  this.script.sh("echo '***********************ENV Setup Done***********************'")
         if (this.config.pompath.length() > 0 && this.config.pompath != false) {
-		this.script.sh("echo '***********************Inside If***********************'")
             String fileName = this.config.pompath.substring(this.config.pompath.lastIndexOf("/"))
             if(fileName.lastIndexOf(".") != -1 && fileName.lastIndexOf(".") != 0) {
                  this.tmp = fileName.substring(fileName.lastIndexOf(".")+1);
             } else {
                  this.tmp = "Something else"
             }
-		
-		this.script.sh("echo '***********************After first if***********************'")
-	    System.out.println(this.config.pompath.length())
             if (this.tmp != 'xml' && this.config.pompath.charAt(this.config.pompath.length()-1) != File.separatorChar) {
                 this.config.pompath += File.separator;
             }
-		this.script.sh("echo '***********************xml check***********************'")
 		this.script.sh("mvn clean install -DskipTests=true -f " + this.config.pompath)
             if (this.tmp == 'xml') {
                 return "mvn clean install -DskipTests=true -f " + this.config.pompath
+		this.script.sh("mvn clean install -DskipTests=true -f " + this.config.pompath)
             } else {
-		    this.script.sh("echo '***********************execcute maven command***********************'")
-		    this.script.sh("mvn clean install -DskipTests=true -f " + this.config.pompath + "pom.xml")
-		    
-                return "mvn clean install -DskipTests=true -f " + this.config.pompath + "pom.xml"
+		    this.script.sh("mvn clean install -DskipTests=true -f " + this.config.pompath + "pom.xml")	    
+                    return "mvn clean install -DskipTests=true -f " + this.config.pompath + "pom.xml"
             }
         } else {
-		this.script.sh("echo '***********************else part***********************'")
+		this.script.sh("mvn clean install -DskipTests=true")
             return "mvn clean install -DskipTests=true"
         }
     } else if (this.config.testcase == false) {
@@ -61,11 +53,14 @@ class Maven {
 
             if (extension == 'xml') {
                 return "mvn clean install -DskipTests=false -f " + this.config.pompath
+		this.script.sh("mvn clean install -DskipTests=true -f " + this.config.pompath)
             } else {
                 return "mvn clean install -DskipTests=false -f " + this.config.pompath + "pom.xml"
+		this.script.sh("mvn clean install -DskipTests=true -f " + this.config.pompath + "pom.xml")
             }
         } else {
             return "mvn clean install -DskipTests=false"
+	    this.script.sh("mvn clean install -DskipTests=true")
         }
     } else {
         this.script.error "testcase parameter has invalid value!"
