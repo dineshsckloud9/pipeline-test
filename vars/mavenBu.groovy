72eee6bd166bc18856d6760cd4fae5a2bda66482
Dinesh <dinesh.sc@kloud9.nyc>
2018-02-10 18:28:36 +0530

    Modifying extended shared library for maven build 2

--- vars/mavenBu.groovy
+++ vars/mavenBu.groovy
@@ -10,14 +10,14 @@ def call(body) {
 	node {
 		stage ('Build') {
 		  def fileType = sh(
-			script: "file -z ${config.pomconfpath} | tr -s ' ' | cut -d ' ' -f 2 > /tmp/test",
+			script: "file -z ${config.pomconfpath} | tr -s ' ' | cut -d ' ' -f 2",
 			returnStdout: true
 			)
 			echo "Filetype is: ${fileType}"
-		String str = "XML"
+		String str = "XML$"
 //		String str1 = "${fileType}"
 //			sh ("echo -n ${str1} | sha256sum")
-			sh ("echo -n ${str} | sha256sum")
+//			sh ("echo -n ${str} | sha256sum")
 		if ( "${fileType}" == "${str}" ) {
 			if ( "${config.testcase}" == true ) {
 						sh("mvn clean install -DskipTests=true -f ${config.pomconfpath}")
