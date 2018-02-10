f0500d0aec6533af00f9ca7611be95e51d73c01d
Dinesh <dinesh.sc@kloud9.nyc>
2018-02-10 13:48:34 +0530

    Modifying extended shared library for maven build 1

--- vars/mavenBu.groovy
+++ vars/mavenBu.groovy
@@ -9,18 +9,35 @@ def call(body) {
 //	   agent any
 	node {
 		stage ('Build') {
-			if ( "$config.testcase" == "true" ) {
-				sh('mvn clean install -DskipTests=true')
-				echo "${config.testcase}"
+		   String fileType=`file -z "${config.pomconfpath}" | awk '{print $2}'`
+		   if ($fileType == "XML") {
+			if ( "$config.testcase" == true ) {
+//                                pompathnum=`echo ${#"${config.pomconfpath}"}`
+//                                if ( $pompathnum > 0 && "${config.pomconfpath}" != false ) {
+//				fileName=`echo ${"${config.pomconfpath}"##*"/"}`
+//					if ( $fileName != "pom.xml" ) {
+						sh('mvn clean install -DskipTests=true' -f "${config.pomconfpath}")
+//					}
+//					else ( $fileName == "pom.xml" ) {
+//						echo "Some problems were encountered while processing the POMs"
+//					}
+				}
+			else if ( "$config.testcase" == false ){
+						sh('mvn clean install -DskipTests=false' -f "${config.pomconfpath}")
+//						echo "${config.testcase}"
 			}
-			else if ( "$config.testcase" == "false" ){
-				echo "$config.testcase"
-				sh('mvn clean install -DskipTests=false')
-			}
+//			}
+//			else if ( "$config.testcase" == "false" ){
+//				echo "$config.testcase"
+//				sh('mvn clean install -DskipTests=false')
+//			}
 			else {
 				echo "invalid testcase value"
 				exit 1
 			}
            	}
+		else {
+			echo "Some problems were encountered while processing the POMs"
+		}
 	}
 }
