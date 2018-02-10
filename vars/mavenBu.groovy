8aaeff3473803c0d0b9ce7a8d67d3f7c561f4d6a
Dinesh <dinesh.sc@kloud9.nyc>
2018-02-10 16:35:22 +0530

    Modifying extended shared library for maven build 1

--- vars/mavenBu.groovy
+++ vars/mavenBu.groovy
@@ -14,28 +14,14 @@ def call(body) {
 			returnStdout: true
 			)
 			echo "file type is: ${fileType}"
-		   if ( "${fileType}" == XML ) {
+		   if ("${fileType}" == 'XML') {
 			if ("${config.testcase}" == true) {
-//                                pompathnum=`echo ${#"${config.pomconfpath}"}`
-//                                if ( $pompathnum > 0 && "${config.pomconfpath}" != false ) {
-//				fileName=`echo ${"${config.pomconfpath}"##*"/"}`
-//					if ( $fileName != "pom.xml" ) {
 						sh("mvn clean install -DskipTests=true -f ${config.pomconfpath}")
 						echo "${config.testcase}"
-//					}
-//					else ( $fileName == "pom.xml" ) {
-//						echo "Some problems were encountered while processing the POMs"
-//					}
 				}
 			else if ("${config.testcase}" == false){
 						sh("mvn clean install -DskipTests=false -f ${config.pomconfpath}")
-//						echo "${config.testcase}"
 			}
-//			}
-//			else if ( "$config.testcase" == "false" ){
-//				echo "$config.testcase"
-//				sh('mvn clean install -DskipTests=false')
-//			}
 			else {
 				echo "invalid testcase value"
 			}
