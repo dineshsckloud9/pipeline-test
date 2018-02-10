68b96f86f4dea7da33eef302ecc2c8e2e9a92af6
Dinesh <dinesh.sc@kloud9.nyc>
2018-02-10 19:46:08 +0530

    Modifying extended shared library for maven build 4

--- vars/mavenBu.groovy
+++ vars/mavenBu.groovy
@@ -5,9 +5,9 @@ def call(body) {
     body.delegate = config
     body()
 
-//	pipeline { 
-//	   agent any
-	node {
+	pipeline { 
+	   agent any
+//	node {
 		stage ('Build') {
 		  def fileType = sh(
 			script: "file -z ${config.pomconfpath} | tr -s ' ' | cut -d ' ' -f 2 | tr -dc A-Z",
@@ -15,11 +15,10 @@ def call(body) {
 			)
 			echo "Filetype is: ${fileType}"
 		if ( "${fileType}" == "XML" ) {
-			if ( "${config.testcase}" == true ) {
+			if ( "${config.testcase}" == "true" ) {
 						sh("mvn clean install -DskipTests=true -f ${config.pomconfpath}")
-						sh("echo ${config.testcase} > /tmp/test")
 				}
-			else if ( "${config.testcase}" == false ) {
+			else if ( "${config.testcase}" == "false" ) {
 						sh("mvn clean install -DskipTests=false -f ${config.pomconfpath}")
 			}
 			else {
